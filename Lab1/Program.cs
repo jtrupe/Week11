e291a8c6ba273811f70c9718b6c47a179d4566f9
jtrupe <53407584+jtrupe@users.noreply.github.com>
2021-11-05 12:21:50 -0700

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,152 @@
+﻿/// Week 11 Lab 1
+///
+/// @author: Julian Trupe
+/// Date:  November 4, 2021
+///
+/// Problem Statement: Create a Document class with text property, Email and File derived classes
+//
+/// Overall Plan:
+/// 1) Create Document class with text property, getter/setter
+/// 2) Create derived Email class with additional sender, recipient, title properties, getters/setters, overridden ToString() method
+/// 3) Create derived File (from Document) class with additional pathname property, getter/setter, overridden ToString() method
+/// 4) Create 2 Email and File objects in the main method and test classes
+/// 5) Pass email and file to ContainsKeyoword method, to validate it works
+
+
+using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Below we will test some classes");
+            Email email1 = new Email();
+            Email email2 = new Email("hello world", "Julian", "Grace", "greeting");
+            File file1 = new File();
+            File file2 = new File("_home", "blah blah blah");
+            Console.WriteLine(email1.ToString());
+            Console.WriteLine(email2.ToString());
+            Console.WriteLine(file1.ToString());
+            Console.WriteLine(file2.ToString());
+
+            Console.WriteLine(ContainsKeyword(email2, "hello"));
+            Console.WriteLine(ContainsKeyword(file2, "blah"));
+        }
+
+        public static bool ContainsKeyword(Document docObject, string keyword)
+        {
+            if (docObject.ToString().IndexOf(keyword, 0) >= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class Document
+    {
+        protected string text;
+
+        public Document()
+        {
+            SetText("sample text");
+        }
+
+        public Document(string txt)
+        {
+            SetText(txt);
+        }
+        
+        public void SetText(string txt)
+        {
+            text = txt;
+        }
+        public string ToString()
+        {
+            return text;
+        }
+    }
+
+    class Email : Document
+    {
+        private string sender;
+        private string recipient;
+        private string title;
+
+        public Email()
+        {
+            SetSender("John Doe");
+            SetRecipient("Jane Doe");
+            SetTitle("Subject");
+            SetText("sample text");
+        }
+        public Email(string txt, string sent, string received, string title)
+        {
+            SetText(txt);
+            SetSender(sent);
+            SetRecipient(received);
+            SetTitle(title);
+        }
+        
+        public string GetSender()
+        {
+            return sender;
+        }
+        public void SetSender(string sent)
+        {
+            sender = sent;
+        }
+        public string GetRecipient()
+        {
+            return recipient;
+        }
+        public void SetRecipient(string received)
+        {
+            recipient = received;
+        }
+        public string GetTitle()
+        {
+            return title;
+        }
+        public void SetTitle(string txt)
+        {
+            title = txt;
+        }
+        public string ToString()
+        {
+            return GetSender() + GetRecipient() + GetTitle() + text;
+        }
+    }
+
+    class File : Document
+    {
+        private string pathname;
+
+        public File()
+        {
+            SetPath("_default");
+            SetText("sample text");
+        }
+        public File(string path, string txt)
+        {
+            SetPath(path);
+            SetText(txt);
+        }
+
+        public void SetPath(string path)
+        {
+            pathname = path;
+        }
+        public string GetPath()
+        {
+            return pathname;
+        }
+
+        public string ToString()
+        {
+            return GetPath() + text;
+        }
+    }
+}
---
+++ project1/Program.cs
@@ -0,0 +1,188 @@
+﻿/// Week 11 Project 1
+///
+/// @author: Julian Trupe
+/// Date:  November 4, 2021
+///
+/// Problem Statement: Create a Vehicle class with owner (Person class), cylinders, manufacturer properties
+///                     Create Truck class derived from Vehicle with additional properties towCapacity and loadCapacity
+//
+/// Overall Plan:
+/// 1) Create Person class, Vehicle class, Truck class derived from Vehicle
+/// 2) Create getters/setters only in classes which require(Truck doesnt need getter/setters for Vehicle properties)
+/// 3) Override ToString methods to print class properties to the console
+/// 4) Create Equals method which returns true if all corresponding properties of a class object are equal
+/// 5) In the main method create at least two of each class, test Equals and ToString methods
+
+
+using System;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Below we will test our classes");
+            Person person1 = new Person();
+            Person person2 = new Person();
+            Console.WriteLine(person1.Equals(person2));
+            Vehicle car1 = new Vehicle();
+            Vehicle car2 = new Vehicle("John Doe", 0, 4);
+            Console.WriteLine(car1.Equals(car2));
+            Truck truck1 = new Truck();
+            Truck truck2 = new Truck(1, 1000);
+            Truck truck3 = new Truck("Julian Trupe", Vehicle.manufacturer.Toyota, 6, 10, 2000);
+            truck3.ToString();
+            Console.WriteLine(truck1.Equals(truck2));
+            Console.WriteLine(truck1.Equals(truck3));
+            truck1.ToString();
+            car1.ToString();
+            person1.ToString();
+        }
+    }
+
+    public class Vehicle
+    {
+        public enum manufacturer { Ford, Toyota, Honda };
+        protected manufacturer manuName;
+        protected int cylinders;
+        protected Person owner;
+
+        public Vehicle()
+        {
+            owner = new Person();
+            manuName = 0;
+            cylinders = 4;
+        }
+
+        public Vehicle(string name, manufacturer manu, int cyl)
+        {
+            owner = new Person(name);
+            manuName = manu;
+            cylinders = cyl;
+        }
+
+        protected void SetManuName(manufacturer name)
+        {
+            manuName = name;
+        }
+        protected manufacturer GetManuName()
+        {
+            return manuName;
+        }
+        protected void SetCylinders(int cyl)
+        {
+            cylinders = cyl;
+        }
+        protected int GetCylinders()
+        {
+            return cylinders;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine(owner.GetName() + "'s " + GetManuName() + " vehicle has " + GetCylinders() + " cylinders");
+        }
+
+        public bool Equals(Vehicle car)
+        {
+            return (car.owner.Equals(owner) && car.GetManuName() == manuName && car.GetCylinders() == cylinders);
+        }
+
+
+    }
+
+    public class Truck : Vehicle
+    {
+        private double loadTons;
+        private int towCapacity;
+
+        public Truck()
+        {
+            owner = new Person();
+            loadTons = 1;
+            towCapacity = 1000;
+            SetCylinders(4);
+            SetManuName(0);
+
+        }
+        public Truck(double load, int tow)
+        {
+            owner = new Person();
+            loadTons = load;
+            towCapacity = tow;
+            SetCylinders(4);
+            SetManuName(0);
+        }
+        public Truck(string ownerName, manufacturer make, int cyl, double load, int tow)
+        {
+            owner = new Person(ownerName);
+            SetManuName(make);
+            SetCylinders(cyl);
+            SetLoad(load);
+            SetTow(tow);
+        }
+
+        public void SetLoad(double num)
+        {
+            loadTons = num;
+        }
+        public double GetLoad()
+        {
+            return loadTons;
+        }
+        public void SetTow(int num)
+        {
+            towCapacity = num;
+        }
+        public int GetTow()
+        {
+            return towCapacity;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine(owner.GetName() + "'s " + GetManuName() + " truck has " + GetCylinders() + " cylinders, " + GetLoad() + " ton load capacity, and " + GetTow() + " pound tow capacity");
+        }
+        public bool Equals(Truck car)
+        {
+            return (car.owner.Equals(owner) && manuName==car.GetManuName() && cylinders==car.GetCylinders() && loadTons==car.GetLoad() && towCapacity==car.GetTow());
+        }
+    }
+
+    public class Person
+    {
+        private string name;
+
+        public Person()
+        {
+            name = "John Doe";
+        }
+        public Person(string parName)
+        {
+            name = parName;
+        }
+        public Person(Person person)
+        {
+            name = person.name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string parName)
+        {
+            name = parName;
+        }
+        public string ToString()
+        {
+            Console.WriteLine(name);
+            return name;
+        }
+        public bool Equals(Person person)
+        {
+            return name == person.name;
+        }
+    }
+}
